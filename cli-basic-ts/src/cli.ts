e02257bbc69d8d85f3329837a695598c9244d409
RolandWarburton <warburtonroland@gmail.com>
2022-02-05 00:18:25 +1100

    Add cli ts boilerplate

---
+++ cli-basic-ts/src/cli.ts
@@ -0,0 +1,11 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { main } from "./index";
+
+export async function cli(processArgs: any) {
+  const args = yargs(hideBin(processArgs)).command("--name <string>", "The user's name").parse();
+
+  let config = {};
+
+  await main(config);
+}
---
+++ cli-basic-ts/src/index.ts
@@ -0,0 +1,5 @@
+async function main(config: any) {
+  console.log("Hello, world!");
+}
+
+export { main };

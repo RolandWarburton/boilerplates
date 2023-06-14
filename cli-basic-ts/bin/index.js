2be7c192011452e1530b4d030a0f6593da505122
RolandWarburton <warburtonroland@gmail.com>
2023-06-14 21:14:25 +1000

    Wait for input before closing

--- cli-basic-ts/bin/index.js
+++ cli-basic-ts/bin/index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
 require("../dist/cli").cli(process.argv);
+
+// wait for input before closing
+console.log('press any key to continue')
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+process.stdin.once('data', () => {
+  process.stdin.setRawMode(false);
+  console.log('Closing...');
+  process.exit(0);
+});

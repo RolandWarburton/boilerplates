afec09b3b2b18478ea8f99703348efda3f83e8f1
RolandWarburton <warburtonroland@gmail.com>
2023-06-15 12:48:19 +1000

    Update main function to return name string

--- cli-basic-ts/src/index.ts
+++ cli-basic-ts/src/index.ts
@@ -1,5 +1,7 @@
-async function main(config: any) {
-  console.log("Hello, world!");
+async function main(config: any): Promise<string> {
+  const { name } = config;
+  console.log(`Hello, ${name}!`);
+  return name;
 }
 
 export { main };

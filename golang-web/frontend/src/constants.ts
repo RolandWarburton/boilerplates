3514583e18f6eb1a7c0c2e03110e7556ee2ff3a3
RolandWarburton <warburtonroland@gmail.com>
2023-08-21 20:10:08 +1000

    Update constants to not use process.env

--- golang-web/frontend/src/constants.ts
+++ golang-web/frontend/src/constants.ts
@@ -1,7 +1,7 @@
-const PROTOCOL: string = (process.env.PROTOCOL as string) || 'https';
-const DOMAIN: string = (process.env.DOMAIN as string) || 'localhost';
-const PORT: number = parseInt(process.env.PORT as string) || 443;
-const API_PORT: number = parseInt(process.env.API_PORT as string) || 443;
-const API_ROOT: string = process.env.API_PORT as string || 'api/v1';
+const PROTOCOL: string = 'https';
+const DOMAIN: string = 'localhost';
+const PORT: number = 443;
+const API_PORT: number = 443;
+const API_ROOT: string = 'api/v1';
 
 export { PROTOCOL, DOMAIN, PORT, API_PORT, API_ROOT };

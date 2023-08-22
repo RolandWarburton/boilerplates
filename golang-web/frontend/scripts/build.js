2ff112fa0a6a161dc66daa3543d44ffddd4271b5
RolandWarburton <warburtonroland@gmail.com>
2023-08-22 13:41:18 +1000

    Update build.js

--- golang-web/frontend/scripts/build.js
+++ golang-web/frontend/scripts/build.js
@@ -1,8 +1,14 @@
+/* eslint-disable no-console */
+//
+// watch and rebuild changes by using below
+// node build.js watch
+//
 const fs = require('fs');
 const sassPlugin = require('esbuild-plugin-sass');
 const { build } = require('esbuild');
 const path = require('path');
 const { promisify } = require('util');
+const { context } = require('esbuild');
 
 const mkdir = promisify(fs.mkdir);
 const readdir = promisify(fs.readdir);
@@ -36,15 +42,20 @@ async function copyDirContents(sourcePath, destPath) {
       }
     }
   } catch (err) {
-    // eslint-disable-next-line
     console.error('Error copying directory contents:', err);
   }
 }
 
 async function main() {
-  let entryFileName;
   makeTemp('build');
-  const result = await build({
+
+  // copy the html root over
+  await copyFile('./public/index.html', './build/index.html');
+
+  // copy the other directory contents over
+  // await copyDirContents('public', 'build');
+
+  const esbuildOptions = {
     entryPoints: ['./src/index.tsx'],
     bundle: true,
     // jsx: 'preserve',
@@ -59,20 +70,43 @@ async function main() {
       '.svg': 'dataurl',
       '.ts': 'tsx'
     }
-    // onEnd: (result) => {
-    //   if (result?.outputFiles?.[0]?.path) {
-    //     const outputPath = result.outputFiles[0].path;
-    //     entryFileName = path.basename(outputPath);
-    //   }
-    // }
-  }).catch((err) => {
-    console.log(err);
-    process.exit(1);
-  });
-  await copyFile('./public/index.html', './build/index.html');
-  // await copyDirContents('public', 'build');
-  // eslint-disable-next-line no-console
-  console.log(result);
+  };
+
+  if (process.argv[2] === 'watch') {
+    const result = await context({
+      ...esbuildOptions
+    }).catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+    result.serve({
+      port: 8000,
+      servedir: 'build',
+      host: '0.0.0.0',
+      keyfile: '../nginx/keys/privkey.pem',
+      certfile: '../nginx/keys/fullchain.pem'
+    });
+    await result.watch();
+    console.log('watching...');
+
+    let timeout;
+    fs.watch('build', (eventType) => {
+      if (eventType === 'change') {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          console.log('Rebuilding...');
+        }, 2000);
+      }
+    });
+  } else {
+    const result = await build({
+      ...esbuildOptions
+    }).catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+    console.log(result);
+  }
 }
 
 main();

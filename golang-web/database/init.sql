6baf63df70beaa4a103cc984888e4d558705e07a
RolandWarburton <warburtonroland@gmail.com>
2023-08-22 18:06:49 +1000

    Seed some example users in init.sql

--- golang-web/database/init.sql
+++ golang-web/database/init.sql
@@ -12,3 +12,14 @@ CREATE TABLE accounts (
 );
 
 ALTER TABLE accounts OWNER TO example;
+
+-- SEED DATABASE
+
+-- password is rhinos
+INSERT INTO accounts (username, password, is_staff)
+VALUES ('roland', '3de2d77d30ef46859f5a088a6c888105e66e97473a09467d02a996f879efa118', TRUE);
+
+-- password is duck
+INSERT INTO accounts (username, password, is_staff)
+VALUES ('rinne', '5189148bef66f4743a8214f0c990adb2fd7661a1990c74c82bece4f8421b1631', TRUE);
+

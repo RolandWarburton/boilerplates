e6569ee4c743b66770c2d3d93422f5d006336c46
RolandWarburton <warburtonroland@gmail.com>
2024-06-06 20:32:05 +1000

    WIP

--- chrome-react-extension/src/forms/options/index.tsx
+++ chrome-react-extension/src/forms/options/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { Input } from '../../components/forms';
 
@@ -11,23 +11,70 @@ function submit(data: Inputs) {
   console.log(data);
 }
 
+const defaultValues = {
+  name: 'roland',
+  age: 0
+};
+
+function getSettings(): Promise<Inputs> {
+  return new Promise((resolve) => {
+    chrome.storage.local.get('settings', async (result) => {
+      if (result.settings) {
+        const settings = result.settings as Inputs;
+        resolve(settings);
+      } else {
+        resolve(defaultValues);
+      }
+    });
+  });
+}
+
+function setDefaultSettings(setFunc: React.Dispatch<Inputs>) {
+  chrome.storage.local.set({ settings: defaultValues }, () => {
+    setFunc(defaultValues);
+  });
+}
+
 function OptionsForm() {
+  const [settings, setSettings] = useState<undefined | Inputs>(undefined);
+
   const {
     register,
     handleSubmit,
     watch,
-    formState: { errors }
+    formState: { errors },
+    setValue
   } = useForm<Inputs>({
-    defaultValues: {
-      name: 'roland',
-      age: 0
-    }
+    defaultValues: defaultValues
   });
 
+  useEffect(() => {
+    const doAsync = async () => {
+      const settings = await getSettings();
+      console.log(settings);
+      if (settings == undefined) {
+        setDefaultSettings(setSettings);
+      } else {
+        setSettings(settings);
+        for (const o of Object.keys(settings)) {
+          setValue(o, settings[o]);
+        }
+      }
+    };
+    doAsync();
+  }, []);
+
   const onSubmit: SubmitHandler<Inputs> = (data) => {
+    chrome.storage.local.set({ settings: data }, () => {
+      setSettings(data);
+    });
     submit(data);
   };
   console.log(watch('name'));
+
+  if (settings == undefined) {
+    return <div>loading</div>;
+  }
 
   return (
     <div>
@@ -43,6 +90,7 @@ function OptionsForm() {
         {errors.age && <span>{errors.age.type}</span>}
         <input type="submit" />
       </form>
+      {JSON.stringify(settings)}
     </div>
   );
 }

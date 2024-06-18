fc32ebf1b6b14c5712eac2bb6ba816618d5da6db
RolandWarburton <warburtonroland@gmail.com>
2024-06-18 18:14:25 +1000

    update build.js lift build to its own function

--- chrome-react-extension/build.js
+++ chrome-react-extension/build.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { build } = require('esbuild');
+const { build: esbuild } = require('esbuild');
 
 function copyFolderSync(source, target) {
   if (!fs.existsSync(target)) {
@@ -29,29 +29,35 @@ function makeTemp(name) {
   }
 }
 
-async function main() {
-  makeTemp('dist');
+const buildSettings = {
+  entryPoints: ['src/index.tsx', 'src/options.tsx'],
+  outdir: './dist',
+  platform: 'node',
+  bundle: true,
+  write: false,
+  format: 'esm',
+  jsx: 'automatic',
+  loader: {
+    '.js': 'jsx',
+    '.ts': 'tsx'
+  },
+  globalName: 'React',
+  define: { 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development') }
+};
 
-  const result = await build({
-    entryPoints: ['src/index.tsx', 'src/options.tsx'],
-    outdir: './dist',
-    platform: 'node',
-    bundle: true,
-    write: false,
-    format: 'esm',
-    jsx: 'automatic',
-    loader: {
-      '.js': 'jsx',
-      '.ts': 'tsx'
-    },
-    globalName: 'React',
-    define: { 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development') }
-  }).catch((err) => {
+async function build() {
+  const result = await esbuild(buildSettings).catch((err) => {
     console.log(err);
     process.exit(1);
   });
+  return result;
+}
 
-  let content = ""
+async function main() {
+  makeTemp('dist');
+  const result = await build();
+
+  let content = '';
   content = result.outputFiles[0].text;
   fs.writeFileSync('dist/index.js', content);
   content = result.outputFiles[1].text;

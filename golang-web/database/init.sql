cdb7df2c4b1c1b95aa406a7dfb355de57a7b56e7
RolandWarburton <warburtonroland@gmail.com>
2024-07-18 14:15:05 +1000

    Add comment to database.sql

--- golang-web/database/init.sql
+++ golang-web/database/init.sql
@@ -16,10 +16,12 @@ ALTER TABLE accounts OWNER TO example;
 -- SEED DATABASE
 
 -- password is rhinos
+-- printf 'rhinos' | sha256sum | cut -d '-' -f1 | xargs
 INSERT INTO accounts (username, password, is_staff)
 VALUES ('roland', 'aa342a6b087d9fe25e96457873ca611b35158223281c1985a11cf9ad26f89ec0', TRUE);
 
 -- password is duck
+-- printf 'duck' | sha256sum | cut -d '-' -f1 | xargs
 INSERT INTO accounts (username, password, is_staff)
 VALUES ('rinne', '2d2370db2447ff8cf4f3accd68c85aa119a9c893effd200a9b69176e9fc5eb98', TRUE);
 
